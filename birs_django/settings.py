9e051c13dcedc0e00cb111b0bb259a3355c028c9
sonnenas86-eng <sonnenas86@gmail.com>
2026-05-08 15:34:42 +0100

    Fix database configuration with dj_database_url

--- birs_django/settings.py
+++ birs_django/settings.py
@@ -71,12 +71,13 @@ TEMPLATES = [
     },
 ]
 
-DATABASE_URL=...
+
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL'),
-        conn_max_age=600
+    'default': dj_database_url.parse(
+        env('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
     )
 }
 
