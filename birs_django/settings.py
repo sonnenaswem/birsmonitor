5c3b369b8025ab280446ea93613f9e80a80e59c9
sonnenas86-eng <sonnenas86@gmail.com>
2026-05-10 10:59:29 +0100

    Add backend domain to CSRF_TRUSTED_ORIGINS

--- birs_django/settings.py
+++ birs_django/settings.py
@@ -134,7 +134,7 @@ CORS_ALLOWED_ORIGINS = [
 
 CSRF_TRUSTED_ORIGINS = os.getenv(
     "CSRF_TRUSTED_ORIGINS",
-    "http://localhost:3000,http://127.0.0.1:3000,https://birs-frontend.onrender.com"
+    "http://localhost:3000,http://127.0.0.1:3000,https://birs-frontend.onrender.com,https://birs-backend.onrender.com"
 ).split(",")
 
 CORS_ALLOW_CREDENTIALS = True
